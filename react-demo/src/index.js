2434d77cfe76b38d848c7f134fe536741f88cbfa
yangy <314461947@qq.com>
2020-08-25 09:33:32 +0800

    登录界面元素

--- react-demo/src/index.js
+++ react-demo/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Login from './pages/Login';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <App />,
+  <Login />,
   document.getElementById('root')
 );
 
